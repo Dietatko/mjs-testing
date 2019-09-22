4834cc04b481b0992af495b87d832efe3da16bb0
Andrej Burger <burgera@impruvit.net>
2019-09-22 18:30:05 +0200

    Approximate and NaN assertions added

--- fs/mjs_assert.js
+++ fs/mjs_assert.js
@@ -1,3 +1,5 @@
+load("mjs_number.js");
+
 function assertIsObject(c, val, ac) {
    if (typeof ac !== "object") {
       c.outcome = false;
@@ -29,6 +31,36 @@ function assertIsNotEqual(c, val, ac, ex) {
    if (ac === ex) {
       c.outcome = false;
       c.error = "The '" + val + "' was '" + JSON.stringify(ac) + "' while not expected.";
+      return false;
+   }
+   return true;
+}
+
+function assertIsApproxEqual(c, val, ac, ex, eps) {
+   if (eps === undefined) {
+      eps = Number.EPSILON;
+   }
+   if (Math.abs(ac - ex) > eps) {
+      c.outcome = false;
+      c.error = "The '" + val + "' was '" + JSON.stringify(ac) + "' while " + JSON.stringify(ex) + " +- " + JSON.stringify(eps) + "' was expected.";
+      return false;
+   }
+   return true;
+}
+
+function assertIsNaN(c, val, ac) {
+   if (!isNaN(ac)) {
+      c.outcome = false;
+      c.error = "The '" + val + "' was '" + JSON.stringify(ac) + "' while NaN was expected.";
+      return false;
+   }
+   return true;
+}
+
+function assertIsNotNaN(c, val, ac) {
+   if (isNaN(ac)) {
+      c.outcome = false;
+      c.error = "The '" + val + "' was NaN while non-NaN was expected.";
       return false;
    }
    return true;
